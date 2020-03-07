c0a9a2143ae13417d9d6d9eab04e4786b1f5db43
KostyaJRZ <54120489+KostyaJRZ@users.noreply.github.com>
2020-03-07 19:43:55 +0200

    Settings: Fix codename prop

--- src/com/android/settings/deviceinfo/firmwareversion/AboutDeviceNamePreferenceController.java
+++ src/com/android/settings/deviceinfo/firmwareversion/AboutDeviceNamePreferenceController.java
@@ -26,7 +26,7 @@ public class AboutDeviceNamePreferenceController extends BasePreferenceControlle
 
     private static final String TAG = "AboutDeviceNameCtrl";
 
-    private static final String KEY_DEVICE_NAME_PROP = "org.evolution.device";
+    private static final String KEY_DEVICE_NAME_PROP = "org.pixelplusui.device";
 
     public AboutDeviceNamePreferenceController(Context context, String key) {
         super(context, key);
